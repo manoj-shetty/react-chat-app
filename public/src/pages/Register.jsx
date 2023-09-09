16c30ea967cac62528142948f62cb1cf8b50fc71
manoj-shetty <manojs231@outlook.com>
2023-09-09 22:30:51 +0530

    Added toast and form validation for register page

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -2,64 +2,100 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+import { registerRoute } from "../utils/APIRoutes";
+
 const Register = () => {
 
-    const [values, setValues] = useState({
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    });
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("form");
-    };
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
 
-    const handleChange = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value })
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { password, confirmPassword, username, email } = values;
+      const { data } = await axios.post(registerRoute, { username, email, password, });
+    }
+  };
 
-    return (
-        <>
-            <FormContainer>
-                <form onSubmit={(event) => handleSubmit(event)}>
-                    <div className="brand">
-                        <img src={Logo} alt="Logo" />
-                        <h1>mchat</h1>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        onChange={(e) => handleChange(e)}
-                    />
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        onChange={(e) => handleChange(e)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        onChange={(e) => handleChange(e)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Confirm Password"
-                        name="confirmPassword"
-                        onChange={(e) => handleChange(e)}
-                    />
-                    <button type="submit">Create User</button>
-                    <span>
-                        Already have an account? <Link to="/login">Login</Link>
-                    </span>
-                </form>
-            </FormContainer>
-        </>
-    );
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error("Password and Confirm Password should be same.", toastOptions);
+      return false;
+    } else if (username.length < 3) {
+      toast.error("Username should be greater than 3 characters", toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error("Password should be equal or greater than 8 characters", toastOptions);
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value })
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="Logo" />
+            <h1>mchat</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
 };
 
 const FormContainer = styled.div`
