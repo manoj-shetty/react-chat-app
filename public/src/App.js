578b3316c1162076474f532f10c95c0ec25a52fb
manoj-shetty <manojs231@outlook.com>
2023-09-13 16:04:44 +0530

    Added react suspense and lazy api for optimizing the app

--- public/src/App.js
+++ public/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import SetAvatar from './pages/SetAvatar';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Register from './pages/Register';
+// import Login from './pages/Login';
+// import Chat from './pages/Chat';
+// import SetAvatar from './pages/SetAvatar';
+const Chat = lazy(() => import("./pages/Chat"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const SetAvatar = lazy(() => import("./pages/SetAvatar"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/setAvatar' element={<SetAvatar/>}></Route>
-        <Route path='/' element={<Chat/>}></Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/setAvatar' element={<SetAvatar />}></Route>
+          <Route path='/' element={<Chat />}></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
